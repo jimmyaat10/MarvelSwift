6206a7053eab0c6c7099247495ee678bf9f9b09f
Albert Arroyo Trabal <jimmy.aat10@gmail.com>
2016-09-06 14:44:11 +0200

    added ThumbnailModelSpec

---
+++ MarvelTests/Models/ThumbnailModelSpec.swift
@@ -0,0 +1,32 @@
+//
+//  ThumbnailModelSpec.swift
+//  Marvel
+//
+//  Created by Albert Arroyo on 6/9/16.
+//  Copyright © 2016 AlbertArroyo. All rights reserved.
+//
+
+import Quick
+import Nimble
+import SwiftyJSON
+
+@testable import Marvel
+
+class ThumbnailModelSpec: QuickSpec {
+    
+    override func spec() {
+        let path = "http://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784"
+        let ext = "jpg"
+        let data:[String: AnyObject] =  ["path" : path , "extension" : ext]
+        var thumb: ThumbnailModel!
+        
+        beforeEach {
+            thumb = ThumbnailModel(json: SwiftyJSON.JSON(data))
+        }
+        it("Thumbnail Model converts from JSON") {
+           	expect(thumb.path) == path
+            expect(thumb.ext) == ext
+        }
+    }
+}
+
